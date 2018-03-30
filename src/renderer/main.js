bf9233e3385c5c317ad7055209043e68b2e3c344
Andy Tan <andytan0727@gmail.com>
2018-03-30 11:52:16 +0800

    Remove variable assignment to Vue object

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -9,7 +9,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-var vm = new Vue({
+new Vue({
   components: { App },
   router,
   store,
