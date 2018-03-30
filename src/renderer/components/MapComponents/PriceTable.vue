050039b24aa833722de55cf967b752760a17bddd
Andy Tan <andytan0727@gmail.com>
2018-03-30 14:30:38 +0800

    Added non-breaking space in table, change dollar sign to land column, set align right in price column

--- src/renderer/components/MapComponents/PriceTable.vue
+++ src/renderer/components/MapComponents/PriceTable.vue
@@ -1,21 +1,19 @@
 <template>
-  <div id="outerPriceTableDiv">
     <div id="priceTableVueDiv">
       <table>
         <tbody>
           <tr v-for="land in lands"
               :key="land.region">
-            <td class="tableRegion">
+            <td id="tableRegion">
               {{ land.region }}
             </td>
             <td id="tablePrice">
-              {{ land.price }}
+              {{ land.price.replace(/ /g, '\u00a0') }}
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-  </div>
 </template>
 
 <script>
@@ -27,51 +25,51 @@ export default {
     return {
       path: '',
       lands: [
-        {price: "$0", region: 'land1:'},
-        {price: "$0", region: 'land2:'},
-        {price: "$0", region: 'land3:'},
-        {price: "$0", region: 'land4:'},
-        {price: "$0", region: 'land5:'},
-        {price: "$0", region: 'land6:'},
-        {price: "$0", region: 'land7:'},
-        {price: "$0", region: 'land8:'},
-        {price: "$0", region: 'land9:'},
-        {price: "$0", region: 'land10:'},
-        {price: "$0", region: 'land11:'},
-        {price: "$0", region: 'land12:'},
-        {price: "$0", region: 'land13:'},
-        {price: "$0", region: 'land14:'},
-        {price: "$0", region: 'land15:'},
-        {price: "$0", region: 'land16:'},
-        {price: "$0", region: 'land17:'},
-        {price: "$0", region: 'land18:'},
-        {price: "$0", region: 'land19:'},
-        {price: "$0", region: 'land20:'},
-        {price: "$0", region: 'land21:'},
-        {price: "$0", region: 'land22:'},
-        {price: "$0", region: 'land23:'},
-        {price: "$0", region: 'land24:'},
-        {price: "$0", region: 'land25:'},
-        {price: "$0", region: 'land26:'},
-        {price: "$0", region: 'land27:'},
-        {price: "$0", region: 'land28:'},
-        {price: "$0", region: 'land29:'},
-        {price: "$0", region: 'land30:'},
-        {price: "$0", region: 'land31:'},
-        {price: "$0", region: 'land32:'},
-        {price: "$0", region: 'land33:'},
-        {price: "$0", region: 'land34:'},
-        {price: "$0", region: 'land35:'},
-        {price: "$0", region: 'land36:'},
-        {price: "$0", region: 'land37:'},
-        {price: "$0", region: 'land38:'},
-        {price: "$0", region: 'land39:'},
-        {price: "$0", region: 'land40:'},
-        {price: "$0", region: 'land41:'},
-        {price: "$0", region: 'land42:'},
-        {price: "$0", region: 'land43:'},
-        {price: "$0", region: 'land44:'},
-        {price: "$0", region: 'land45:'},
+        {price: "0", region: 'land  1: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  2: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  3: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  4: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  5: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  6: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  7: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  8: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land  9: $'.replace(/ /g, '\u00a0')},
+        {price: "0", region: 'land10: $'},
+        {price: "0", region: 'land11: $'},
+        {price: "0", region: 'land12: $'},
+        {price: "0", region: 'land13: $'},
+        {price: "0", region: 'land14: $'},
+        {price: "0", region: 'land15: $'},
+        {price: "0", region: 'land16: $'},
+        {price: "0", region: 'land17: $'},
+        {price: "0", region: 'land18: $'},
+        {price: "0", region: 'land19: $'},
+        {price: "0", region: 'land20: $'},
+        {price: "0", region: 'land21: $'},
+        {price: "0", region: 'land22: $'},
+        {price: "0", region: 'land23: $'},
+        {price: "0", region: 'land24: $'},
+        {price: "0", region: 'land25: $'},
+        {price: "0", region: 'land26: $'},
+        {price: "0", region: 'land27: $'},
+        {price: "0", region: 'land28: $'},
+        {price: "0", region: 'land29: $'},
+        {price: "0", region: 'land30: $'},
+        {price: "0", region: 'land31: $'},
+        {price: "0", region: 'land32: $'},
+        {price: "0", region: 'land33: $'},
+        {price: "0", region: 'land34: $'},
+        {price: "0", region: 'land35: $'},
+        {price: "0", region: 'land36: $'},
+        {price: "0", region: 'land37: $'},
+        {price: "0", region: 'land38: $'},
+        {price: "0", region: 'land39: $'},
+        {price: "0", region: 'land40: $'},
+        {price: "0", region: 'land41: $'},
+        {price: "0", region: 'land42: $'},
+        {price: "0", region: 'land43: $'},
+        {price: "0", region: 'land44: $'},
+        {price: "0", region: 'land45: $'},
       ]
     }
   },
@@ -94,7 +92,7 @@ export default {
           // make a 2D array of fileData 
           for (let i = 0; i < fileData.length - 1; i++) {
             fileData[i] = fileData[i].split(",");
-            priceTabObj.lands[i].price = '$' + fileData[i][1]
+            priceTabObj.lands[i].price = fileData[i][1]
           }
         })
       }
@@ -114,28 +112,25 @@ td {
 
 }
 
-#outerPriceTableDiv {
-  display: flex;
-  flex-flow: column;
-  width: 300px;
-}
-
 #priceTableVueDiv {
   column-count:2;
   -webkit-column-count:2;
   
-  width: 240px;
+  width: 250px;
   height: 647px;
   padding-top: 200px;
-  margin-left: -40px;
+  margin-left: -48px;
 
 }
 
-.tableRegion {
+#tableRegion {
   text-align: right;
   padding-left: 2px;
   padding-right: 8px;
 }
 
+#tablePrice {
+  text-align: right;
+}
 </style>
 
