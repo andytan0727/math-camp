c8f9af1d47ced5ec36795e0c35dd85327edc41ef
andytan <andytan0727@gmail.com>
2018-04-12 21:22:33 +0800

    Changed colors. Refactored with Prettier

--- src/renderer/scripts/colors.js
+++ src/renderer/scripts/colors.js
@@ -1,29 +1,28 @@
-'use strict'
+"use strict";
 
 const fs = require("fs");
-const { dialog } = require('electron').remote
+const { dialog } = require("electron").remote;
 
 /// const dataPath = process.cwd() + "/StoreLand.dat";
 var regions;
 const colors = [
-  '',
-  '#c93756',  // group one's color
-  '#f97d1b',     // group two's color
-  '#fff143',   // group three's color
-  '#00e079',    // group four's color
-  '#7df3ff',     // group five's color
-  '#8d4bbb',   // group six's color
-  '#ff0097',   // group seven's color
-  '#984400',     // group eight's color
-  '#eacd76',  // group nine's color
-  '#758a99'   // group ten's color
-]
+  "",
+  "#ff0000", // group one's color
+  "#f97d1b", // group two's color
+  "#ffe400", // group three's color
+  "#00e079", // group four's color
+  "#7df3ff", // group five's color
+  "#8d4bbb", // group six's color
+  "#ff0097", // group seven's color
+  "#984400", // group eight's color
+  "#ffd86d", // group nine's color
+  "#758a99" // group ten's color
+];
 
 function setRegions() {
-
   let fileData = fs.readFileSync(global.path[0], "utf8").split("\n");
 
-  // make a 2D array of fileData 
+  // make a 2D array of fileData
   for (let i = 0; i < fileData.length; i++) {
     fileData[i] = fileData[i].split(",");
   }
@@ -34,7 +33,7 @@ function setRegions() {
 function setRegionColors(id) {
   let blockId = "block" + id;
   let block = document.getElementById(blockId);
-  block.style.fill = colors[regions[id - 1][2]]
+  block.style.fill = colors[regions[id - 1][2]];
 }
 
 export function watchFileChanges() {
@@ -42,12 +41,12 @@ export function watchFileChanges() {
     try {
       setRegions();
     } catch (err) {
-      console.log(err)
+      console.log(err);
       return;
     }
 
     for (let i = 1; i <= 45; i++) {
       setRegionColors(i);
-    }  
-  })
+    }
+  });
 }
